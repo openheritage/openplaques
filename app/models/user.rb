0a48f3e17d51790ee54aa79a5fa4af80388d9c7d
Frankie Roberto <frankie@frankieroberto.com>
2011-03-29 21:26:14 +0100

    Some basic styles for fields with errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   
   has_many :plaques
   
