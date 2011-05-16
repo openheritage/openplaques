58b6b86d9310dd5dca155e67f0fd1014c1f965a9
Frankie Roberto <frankie@frankieroberto.com>
2011-05-16 19:20:54 +0100

    Removing redundant tests

--- test/functional/homepage_controller_test.rb
+++ test/functional/homepage_controller_test.rb
@@ -8,11 +8,7 @@ class HomepageControllerTest < ActionController::TestCase
     end
     
     should respond_with :success
-    should assign_to :recent_photos
-    should assign_to :geolocated_plaques_count
     should assign_to :plaques_count
-    should assign_to :photographed_plaques_count
-    should assign_to :coloured_plaques_count            
     should render_template :index
     
   end
