c393920071c894d99779705d276096ebf3530192
Frankie Roberto <frankie@frankieroberto.com>
2011-05-18 19:57:12 +0100

    Basic map styling

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -7,14 +7,37 @@ module MapsHelper
     colour = options[:colour] || "blue"
     zoom = options[:zoom] || 16
     maptype = options[:maptype] || nil
+
+    params = []
+
+    options[:styles].each_pair do |feature, value|
+      style = "style=feature:" + feature.to_s
+
+      value.each_pair do |element, value|
+
+        style += "|element:" + element.to_s
+        
+        rules = []
+        
+        value.each_pair do |rule, value|
+          rules << rule.to_s + ":" + value.to_s
+        end
+      
+        style += "|" + rules.join("|")
+      end
+      
+      params << style
+      
+    end  
     
     url = "http://maps.google.com/maps/api/staticmap?"
-    params = []
     params << "size=" + width.to_s + "x" + height.to_s
     params << "markers=color:" + colour + "|" + latitude.to_s + "," + longitude.to_s
     params << "sensor=false"
     params << "zoom=" + zoom.to_s
     params << "maptype=" + maptype if maptype
+
+
     url + params.join("&")
     
   end
