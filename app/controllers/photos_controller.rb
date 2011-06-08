7c56efadca825b475935c50a9e4b69803b4cbfde
Frankie Roberto <frankie@frankieroberto.com>
2011-06-08 23:22:19 +0100

    bugfixes and tests for photo editing pages

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
 
   before_filter :authenticate_admin!, :only => :destroy
-  before_filter :authorisation_required, :except => [:index, :show]
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   before_filter :find_photo, :only => [:destroy, :edit, :show, :update]
 
@@ -34,6 +34,7 @@ class PhotosController < ApplicationController
     if @photo.save
       redirect_to photo_path(@photo)
     else
+      @licences = Licence.all(:order => :name)
       render :new
     end
   end
--- test/functional/photos_controller_test.rb
+++ test/functional/photos_controller_test.rb
@@ -21,4 +21,48 @@ class PhotosControllerTest < ActionController::TestCase
 
   end
 
+  context "when signed in as a user" do
+
+    setup { sign_in users(:frankieroberto) }
+
+    context "when viewing the new photo page" do
+
+      setup { get :new }
+
+      should respond_with :success
+      should assign_to :photo
+
+    end
+
+    context "when submitting a valid new photo" do
+      setup do
+        post :create, :photo => {:file_url => "http://test.com/image.png", :licence_id => licences(:ccby).id, :plaque_id => plaques(:frankie_sheffield_plaque).id }
+      end
+
+      should redirect_to("the page for the new photo") { photo_path(assigns(:photo)) }
+    end
+
+    context "when submitting an invalid new photo" do
+
+      setup { post :create }
+
+      should respond_with :success
+      should assign_to :photo
+      should render_template :new
+
+    end
+
+    context "when updating an existing photo with valid new details" do
+
+      setup do
+        @photo = photos(:frankie_photo)
+        put :update, :id => @photo.id, :photo => {:file_url => @photo.file_url + "?test", :licence_id => @photo.licence_id, :plaque_id => @photo.plaque_id}
+      end
+
+      should redirect_to("the page for the photo") { photo_path(@photo.id) }
+
+    end
+
+  end
+
 end
