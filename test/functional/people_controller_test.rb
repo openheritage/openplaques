3e828ca9ec1a8203c2dc913a58cba335c2edd83b
Frankie Roberto <frankie@frankieroberto.com>
2011-06-28 09:43:08 +0100

    Updating edit people page

--- test/functional/people_controller_test.rb
+++ test/functional/people_controller_test.rb
@@ -6,20 +6,17 @@ class PeopleControllerTest < ActionController::TestCase
     setup do
       get(:index)
     end
-    
     should redirect_to("the 'people beginning with a' page") {people_by_index_path("a")}
-    
   end
-
 
   context "when viewing a person page" do
     setup do
       get(:show, {:id => people(:frankie).id})
     end
-    
+
     should respond_with :success
     should assign_to :person
-    
+
   end
 
   should "get a 404 when requesting non-existant person" do
@@ -27,6 +24,26 @@ class PeopleControllerTest < ActionController::TestCase
     assert_raises(ActiveRecord::RecordNotFound) do
       get(:show, {:id => "99999"})
     end
+  end
+
+  context "when not signed in" do
+
+
+  end
+
+  context "when signed in as a user" do
+
+    setup { sign_in users(:frankieroberto) }
+
+    context "when viewing the page to edit a person" do
+
+      setup { get :edit, :id => people(:frankie).id }
+
+      should respond_with :success
+      should assign_to :person
+
+    end
+
   end
 
 end
