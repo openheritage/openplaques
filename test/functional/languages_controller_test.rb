76cbc275d610c31079e441038b124e2a38cc8b43
Frankie Roberto <frankie@frankieroberto.com>
2011-06-28 22:05:18 +0100

    Updated test

--- test/functional/languages_controller_test.rb
+++ test/functional/languages_controller_test.rb
@@ -5,29 +5,29 @@ class LanguagesControllerTest < ActionController::TestCase
   context "when requesting a non-existant language" do
 
     should "raise a not-found error" do
-      assert_raises(ActiveRecord::RecordNotFound) do      
+      assert_raises(ActiveRecord::RecordNotFound) do
         get(:show, {:id => "zz"})
       end
-    end  
+    end
   end
-  
+
   context "when viewing languages index page" do
     setup { get :index }
-        
+
     should respond_with :success
     should render_template :index
-    
+
     should "have a meaningful page title" do
-      assert_select 'h1', 'Languages'
+      assert_select 'h1', 'Plaques - by language'
     end
-    
+
   end
 
   context "when vieiwng a language page" do
     setup do
       get(:show, {:id => languages(:english).alpha2})
     end
-    
+
     should respond_with :success
     should assign_to :language
     should render_template :show
@@ -39,10 +39,10 @@ class LanguagesControllerTest < ActionController::TestCase
       @language = languages(:english)
       get :show, {:id => @language.id}
     end
-    
+
     should respond_with :moved_permanently
     should redirect_to("language page") {language_path(@language.alpha2)}
-    
+
   end
 
 end
