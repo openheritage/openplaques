d53c52361f8fd071f5c0f637d6dfc3b9ac7c756c
Marvin Barretto <marvinbarretto@gmail.com>
2011-07-08 01:43:15 +0100

    Improved js to trigger onload as well as onwidth change

--- public/javascripts/ToggleSearchValue.js
+++ public/javascripts/ToggleSearchValue.js
@@ -3,12 +3,16 @@ $(document).ready(function() {
 		var smaller_search_value = "Search"
 		var larger_search_value = "Search for a person, place or organisation"
 		
-		$(window).resize( function(){
+		checkWidth();
+		
+		function checkWidth(){
 			var search_cell_width = $(search_cell).width();
 			if (search_cell_width < 370) {
 				$("#phrase").attr("placeholder",smaller_search_value);
 			} else {
 				$("#phrase").attr("placeholder",larger_search_value);
 			}
-		})
+		}
+		
+		$(window).resize(checkWidth);
  });
