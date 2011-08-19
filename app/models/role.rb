b793e3c5a9c08c12a461c4e5da7e186e92a034ec
Jez Nicholson <jez.nicholson@argyllenviro.com>
2011-08-19 14:39:41 +0100

    more 'things'

--- app/models/role.rb
+++ app/models/role.rb
@@ -37,13 +37,16 @@ class Role < ActiveRecord::Base
   def thing?
     false
     #TODO add this as a settable attribute on role
-    return true if "charity" == slug
-    return true if "type_foundry" == slug
-    return true if "football_club" == slug
-    return true if "house" == slug
-    return true if "street" == slug
-    return true if "battle" == slug
     return true if "band" == slug
+    return true if "battle" == slug
+    return true if "charity" == slug
+    return true if "cinema" == slug
+    return true if "football_club" == slug
+    return true if "fire_station" == slug
+    return true if "house" == slug
+    return true if "pier" == slug
+    return true if "street" == slug
+    return true if "type_foundry" == slug
   end
   
   # work it out from the name unless override value stored in the db
