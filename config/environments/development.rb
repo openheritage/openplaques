a5becbb2afe2cca901558aa82dd60280b62ab872
Frankie Roberto <frankie@frankieroberto.com>
2011-09-05 12:16:55 +0100

    more config stuff.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,12 @@
 Openplaques::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
