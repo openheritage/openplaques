3a49040c5b1ddf2d0ff7a1da6bec3c5155816e66
Frankie Roberto <frankie@frankieroberto.com>
2011-09-05 12:07:34 +0100

    Including new asset manifest syntax.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,13 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
@@ -17,9 +27,9 @@ function addLoadEvent(func) {
 
  function addMap(lat,lon,featurecollection) {
 
-﻿   var zoom = 9;﻿  
+﻿   var zoom = 9;﻿
 ﻿   OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3;
-﻿   OpenLayers.Util.onImageLoadErrorColor = "transparent";﻿  
+﻿   OpenLayers.Util.onImageLoadErrorColor = "transparent";﻿
      var options = {
          projection: new OpenLayers.Projection("EPSG:900913"),
          displayProjection: new OpenLayers.Projection("EPSG:4326"),
@@ -38,15 +48,15 @@ function addLoadEvent(func) {
      map.addLayers([mapnik, markers]);
      map.addControl(new OpenLayers.Control.Permalink());
      map.addControl(new OpenLayers.Control.MousePosition());
-     
+
      var proj = new OpenLayers.Projection("EPSG:4326");
 ﻿  ﻿  var point = new OpenLayers.LonLat(lon, lat);
 ﻿  ﻿  point.transform(proj, map.getProjectionObject());
 
      map.setCenter(point, zoom);
-     
+
      if (!map.getCenter()) {map.zoomToMaxExtent()}
-     
+
     var geojson_format = new OpenLayers.Format.GeoJSON(
       {
         'internalProjection': new OpenLayers.Projection("EPSG:900913"),
@@ -59,14 +69,14 @@ function addLoadEvent(func) {
     var size = new OpenLayers.Size(10,17);
     var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
     var icon = new OpenLayers.Icon('http://boston.openguides.org/markers/AQUA.png',size,offset);
-  
+
         point = new OpenLayers.LonLat(-0.07699, 51.521214);
 ﻿  ﻿  point.transform(proj, map.getProjectionObject());
         marker = new OpenLayers.Marker(point,icon.clone());
 ﻿      markers.addMarker(marker);
-      
-    
+
+
  }
- 
+
 
 
