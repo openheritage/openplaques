2ba5f0ada164f0e13d8264a273313b0559410baa
Frankie Roberto <frankie@frankieroberto.com>
2011-10-22 16:06:30 +0100

    Adding caching back in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Openplaques::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = nil
