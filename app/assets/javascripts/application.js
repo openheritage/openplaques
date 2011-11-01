6805c7517b80643c7660deace40a08d80c1d0ba6
Jez Nicholson <jez.nicholson@argyllenviro.com>
2011-11-01 13:03:03 +0000

    map wasn't working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,3 +11,17 @@
 
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
