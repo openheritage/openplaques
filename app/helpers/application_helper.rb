a21457b7f9956b409dac65e9dc5f91e43094c906
Jez Nicholson <jez.nicholson@argyllenviro.com>
2011-12-21 10:18:49 +0000

    attempt to discourage spiders from crawling xml, json, kml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
 
   def alternate_link_to(text, path, format)
-    link_to text, path, :type => Mime::Type.lookup_by_extension(format.to_s).to_s, :rel => :alternate
+    link_to text, path, :type => Mime::Type.lookup_by_extension(format.to_s).to_s, :rel => [:alternate,:nofollow]
   end
 
 
