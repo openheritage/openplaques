c3eb7cee27c41250d151874378262a25a725e3bc
Jez Nicholson <jez.nicholson@gmail.com>
2012-01-27 07:15:43 +0000

    don't fail if no phrase or street given

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,7 +10,7 @@ class SearchController < ApplicationController
         @search_results = Plaque.find(:all, :conditions => ["lower(inscription) LIKE ?", "%" + @phrase.downcase + "%"], :include => [[:personal_connections => [:person]], [:location => [:area => :country]]])
       end
       render "results"
-    else
+    elsif  @street != nil && @street !=""
       @search_results = Plaque.find(:all, :joins => :location, :conditions => ["lower(locations.name) LIKE ?", "%" + @street.downcase + "%"], :include => [[:personal_connections => [:person]], [:location => [:area => :country]]])      
       @phrase = ""
       render "results"
