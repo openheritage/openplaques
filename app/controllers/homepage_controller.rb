7dfde4987f988da36fd53a108d4ca5902a68e509
Jez Nicholson <jez.nicholson@argyllenviro.com>
2012-02-17 12:17:42 +0000

    OpenGraph for home page

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -5,6 +5,16 @@ class HomepageController < ApplicationController
 #    @recent_plaques = Plaque.photographed.order("created_at DESC").limit(2)
     @recent_plaques = Plaque.photographed.random(2)
     @todays = Pick.todays
+    begin
+      set_meta_tags :open_graph => {
+        :type  => :website,
+        :url   => url_for(:only_path=>false),
+        :image => view_context.root_url[0...-1] + view_context.image_path("openplaques-icon.png"),
+        :title => "Open Plaques",
+        :description => "Documenting the historical links between people and places, as recorded by commemorative plaques",
+      }
+    rescue
+    end
   end
 
 end
