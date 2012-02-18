ff60f4f0118df2919f78ed9b5d24d1225d871eff
Jez Nicholson <jez.nicholson@gmail.com>
2012-02-18 16:33:38 +0000

    maps for unphotographed

--- app/controllers/unphotographed_plaques_by_area_controller.rb
+++ app/controllers/unphotographed_plaques_by_area_controller.rb
@@ -3,13 +3,28 @@ class UnphotographedPlaquesByAreaController < ApplicationController
   def show
     @country = Country.find_by_alpha2(params[:country_id])
     @area = @country.areas.find_by_slug(params[:area_id])
-  if @area
     @plaques = @area.plaques.unphotographed
-  end
-    if @plaques
-#   @centre = find_mean(@plaques)
+    @mean = help.find_mean(@plaques)
     @zoom = 11
+    respond_to do |format|
+      format.html
+      format.kml { render "plaques/index" }
+      format.osm { render "plaques/index" }
+      format.yaml
+      format.xml
+      format.json { render :json => @plaques }
     end
   end
+  
+  protected
+
+    def help
+      Helper.instance
+    end
+
+    class Helper
+      include Singleton
+      include PlaquesHelper
+    end
 
 end
--- app/controllers/unphotographed_plaques_by_country_controller.rb
+++ app/controllers/unphotographed_plaques_by_country_controller.rb
@@ -3,6 +3,28 @@ class UnphotographedPlaquesByCountryController < ApplicationController
   def show
     @country = Country.find_by_alpha2!(params[:country_id])
     @plaques = @country.plaques.unphotographed
+    @mean = help.find_mean(@plaques)
+    @zoom = 7
+    respond_to do |format|
+      format.html
+      format.kml { render "plaques/index" }
+      format.osm { render "plaques/index" }
+      format.yaml
+      format.xml
+      format.json { render :json => @plaques }
+    end
   end
+
+
+  protected
+
+    def help
+      Helper.instance
+    end
+
+    class Helper
+      include Singleton
+      include PlaquesHelper
+    end
 
 end
