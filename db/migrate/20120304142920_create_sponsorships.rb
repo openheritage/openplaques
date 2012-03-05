793ffca44f462667007d8bc258556b439e507995
Jez Nicholson <jez.nicholson@argyllenviro.com>
2012-03-05 10:33:41 +0000

    Sponsorships fixes (after testing on a clean install)

--- db/migrate/20120304142920_create_sponsorships.rb
+++ db/migrate/20120304142920_create_sponsorships.rb
@@ -5,10 +5,10 @@ class CreateSponsorships < ActiveRecord::Migration
       t.references :plaque
       t.timestamps
     end
-    Plaque.find(:all).each do |p|
+    Plaque.find(:all, :conditions => ['organisation_id is not null']).each do |p|
       s = Sponsorship.new
       s.plaque = p
-      s.organisation = p.organisation
+      s.organisation_id = p.organisation_id
       s.save
     end
   end
