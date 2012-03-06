73d817c31d07c1dd1834d23922b3cf52be68bc09
Jez Nicholson <jez.nicholson@argyllenviro.com>
2012-03-06 09:01:38 +0000

    name search: "Joseph Swan" -> like "Joseph%Swan%" == "Joseph Wilson Swan"

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,7 +11,7 @@ class SearchController < ApplicationController
       if @street != nil && @street !=""
         @search_results = Plaque.find(:all, :joins => :location, :conditions => ["lower(inscription) LIKE ? and lower(locations.name) LIKE ?", "%" + @phrase.downcase + "%", "%" + @street.downcase + "%"], :include => [[:personal_connections => [:person]], [:location => [:area => :country]]])
       else
-        @search_results = Person.find(:all, :conditions => ["lower(name) LIKE ?", "%" + @phrase.downcase + "%"])
+        @search_results = Person.find(:all, :conditions => ["lower(name) LIKE ?", "%" + @phrase.downcase.gsub(" ","%") + "%"])
         @search_results += Plaque.find(:all, :conditions => ["lower(inscription) LIKE ?", "%" + @phrase.downcase + "%"], :include => [[:personal_connections => [:person]], [:location => [:area => :country]]]).sort!{|t1,t2|t1.to_s <=> t2.to_s}
       end
     elsif  @street != nil && @street !=""
