09ea5520fc54019af6373cde9623308fa9b9c53e
Jez Nicholson <jez.nicholson@gmail.com>
2012-08-05 15:53:30 +0100

    find licence using Flickr's licence id

--- app/models/licence.rb
+++ app/models/licence.rb
@@ -12,5 +12,29 @@ class Licence < ActiveRecord::Base
   validates_uniqueness_of :url
 
   has_many :photos
+  
+  def self.find_by_flickr_licence_id(flickr_licence_id)
+    case flickr_licence_id
+    when "4"
+      return Licence.find_by_url("http://creativecommons.org/licenses/by/2.0/")
+    when "6"
+      return Licence.find_by_url("http://creativecommons.org/licenses/by-nd/2.0/")
+    when "3"
+      return Licence.find_by_url("http://creativecommons.org/licenses/by-nc-nd/2.0/")
+    when "2"
+      return Licence.find_by_url("http://creativecommons.org/licenses/by-nc/2.0/")
+    when "1"
+      return Licence.find_by_url("http://creativecommons.org/licenses/by-nc-sa/2.0/")
+    when "5"
+      return Licence.find_by_url("http://creativecommons.org/licenses/by-sa/2.0/")
+    when "7"
+      return Licence.find_by_url("http://www.flickr.com/commons/usage/")
+    when "0"
+      return Licence.find_by_url("http://en.wikipedia.org/wiki/All_rights_reserved/")
+    else
+      puts "Couldn't find license"
+      return nil
+    end
+  end
 
 end
