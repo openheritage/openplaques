33f03ac6aabff8be3db058cde109cfe04a314c8d
Frankie Roberto <frankie@frankieroberto.com>
2013-12-08 11:56:03 +0000

    Tidying up

--- config/application.rb
+++ config/application.rb
@@ -9,24 +9,8 @@ Bundler.require *Rails.groups(:assets) if defined?(Bundler)
 
 module Openplaques
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # --- INTERNATIONALISATION ---
 
     # Set the default locale to be British English
     config.i18n.default_locale = :"en-GB"
@@ -35,22 +19,25 @@ module Openplaques
     # the I18n.default_locale when a translation can not be found)
     config.i18n.fallbacks = true
 
+
+    # -- ASSETS ---
+
     # Enable the asset pipeline for speedier loading
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # Change the path that assets are served from
-    # config.assets.prefix     = "/assets"
-
-    # This makes Devise work with Heroku
+    # Don't initialize on precompile as this doesn't work with Heroku
     config.assets.initialize_on_precompile = false
 
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
-    # Configure the default encoding used in templates for Ruby 1.9.
+    # --- MISC ---
+
+    # Set default time zone to be London (GMT/BST)
+    config.time_zone = 'London'
+
+    # Default encoding. Use UTF8 everywhere.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
