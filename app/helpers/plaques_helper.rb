e880631cffbfd006614383a315f0d3a416e49b7b
Jez Nicholson <jez.nicholson@gmail.com>
2014-08-17 17:02:16 +0100

    crawl 20 pages of Flickr plaques

--- app/helpers/plaques_helper.rb
+++ app/helpers/plaques_helper.rb
@@ -70,80 +70,83 @@ module PlaquesHelper
     method = "flickr.photos.search"
     license = "1,2,3,4,5,6,7" # All the CC licencses that allow commercial re-use
 
-    url = flickr_url + "?api_key=" + key + "&method=" + method + "&license=" + license + "&content_type=" + content_type + "&machine_tags=" + machine_tag_key +  "&extras=date_taken,owner_name,license,geo,machine_tags"
+    20.times do |page|
 
-    if (flickr_user_id)
-      url += "&user_id=" + flickr_user_id
-    end
-    puts url
+      url = flickr_url + "?api_key=" + key + "&method=" + method + "&page=" + page.to_s + "&license=" + license + "&content_type=" + content_type + "&machine_tags=" + machine_tag_key +  "&extras=date_taken,owner_name,license,geo,machine_tags"
 
-    new_photos_count = 0
-    response = open(url)
-    doc = REXML::Document.new(response.read)
-    doc.elements.each('//rsp/photos/photo') do |photo|
-      print "."
-      $stdout.flush
+      if (flickr_user_id)
+        url += "&user_id=" + flickr_user_id
+      end
+      puts url
 
-      @photo = nil
+      new_photos_count = 0
+      response = open(url)
+      doc = REXML::Document.new(response.read)
+      doc.elements.each('//rsp/photos/photo') do |photo|
+        print "."
+        $stdout.flush
 
-      file_url = "http://farm" + photo.attributes["farm"] + ".staticflickr.com/" + photo.attributes["server"] + "/" + photo.attributes["id"] + "_" + photo.attributes["secret"] + "_z.jpg"
-      photo_url = "http://www.flickr.com/photos/" + photo.attributes["owner"] + "/" + photo.attributes["id"] + "/"
+        @photo = nil
 
-      @photo = Photo.find_by_url(photo_url)
+        file_url = "http://farm" + photo.attributes["farm"] + ".staticflickr.com/" + photo.attributes["server"] + "/" + photo.attributes["id"] + "_" + photo.attributes["secret"] + "_z.jpg"
+        photo_url = "http://www.flickr.com/photos/" + photo.attributes["owner"] + "/" + photo.attributes["id"] + "/"
 
-      if @photo
-      else
-        plaque_id = photo.attributes["machine_tags"][/openplaques\:id\=(\d+)/, 1]
+        @photo = Photo.find_by_url(photo_url)
 
-        puts photo.attributes["title"]
-
-        @plaque = Plaque.find(:first, :conditions => {:id => plaque_id})
-        if @plaque
-          @photo = Photo.new
-          @photo.plaque = @plaque
-          @photo.file_url = file_url
-          @photo.url = photo_url
-          @photo.taken_at = photo.attributes["datetaken"]
-          @photo.photographer_url = photo_url = "http://www.flickr.com/photos/" + photo.attributes["owner"] + "/"
-          @photo.photographer = photo.attributes["ownername"]
-          if photo.attributes["license"] == "4"
-            @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by/2.0/")
-          elsif photo.attributes["license"] == "6"
-            @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-nd/2.0/")
-          elsif photo.attributes["license"] == "3"
-            @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-nc-nd/2.0/")
-          elsif photo.attributes["license"] == "2"
-            @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-nc/2.0/")
-          elsif photo.attributes["license"] == "1"
-            @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-nc-sa/2.0/")
-          elsif photo.attributes["license"] == "5"
-            @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-sa/2.0/")
-          elsif photo.attributes["license"] == "7"
-            @photo.licence = Licence.find_by_url("http://www.flickr.com/commons/usage/")
-          elsif photo.attributes["license"] == "0"
-            @photo.licence = Licence.find_by_url("http://en.wikipedia.org/wiki/All_rights_reserved/")
-          else
-            puts "Couldn't find license"
-          end
-          if @photo.save
-            new_photos_count += 1
-            puts "New photo found and saved"
-          else
-#            puts "Error saving photo" + @photo.errors.each_full{|msg| puts msg }
-          end
-
-          if photo.attributes["latitude"] != "0" && photo.attributes["longitude"] != "0" && !@plaque.geolocated?
-            puts "New geolocation found"
-            @plaque.latitude = photo.attributes["latitude"]
-            @plaque.longitude = photo.attributes["longitude"]
-            if @plaque.save
-              puts "New geolocation added to photo"
-            else
-              puts "Error adding geolocation to photo" + plaque.errors.full_messages.to_s #methods.join(" ")
-            end
-          end
+        if @photo
         else
-          puts "Photo's machine tag doesn't match a plaque."
+          plaque_id = photo.attributes["machine_tags"][/openplaques\:id\=(\d+)/, 1]
+
+          puts photo.attributes["title"]
+
+          @plaque = Plaque.find(:first, :conditions => {:id => plaque_id})
+          if @plaque
+            @photo = Photo.new
+            @photo.plaque = @plaque
+            @photo.file_url = file_url
+            @photo.url = photo_url
+            @photo.taken_at = photo.attributes["datetaken"]
+            @photo.photographer_url = photo_url = "http://www.flickr.com/photos/" + photo.attributes["owner"] + "/"
+            @photo.photographer = photo.attributes["ownername"]
+            if photo.attributes["license"] == "4"
+              @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by/2.0/")
+            elsif photo.attributes["license"] == "6"
+              @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-nd/2.0/")
+            elsif photo.attributes["license"] == "3"
+              @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-nc-nd/2.0/")
+            elsif photo.attributes["license"] == "2"
+              @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-nc/2.0/")
+            elsif photo.attributes["license"] == "1"
+              @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-nc-sa/2.0/")
+            elsif photo.attributes["license"] == "5"
+              @photo.licence = Licence.find_by_url("http://creativecommons.org/licenses/by-sa/2.0/")
+            elsif photo.attributes["license"] == "7"
+              @photo.licence = Licence.find_by_url("http://www.flickr.com/commons/usage/")
+            elsif photo.attributes["license"] == "0"
+              @photo.licence = Licence.find_by_url("http://en.wikipedia.org/wiki/All_rights_reserved/")
+            else
+              puts "Couldn't find license"
+            end
+            if @photo.save
+              new_photos_count += 1
+              puts "New photo found and saved"
+            else
+#            puts "Error saving photo" + @photo.errors.each_full{|msg| puts msg }
+            end
+
+            if photo.attributes["latitude"] != "0" && photo.attributes["longitude"] != "0" && !@plaque.geolocated?
+              puts "New geolocation found"
+              @plaque.latitude = photo.attributes["latitude"]
+              @plaque.longitude = photo.attributes["longitude"]
+              if @plaque.save
+                puts "New geolocation added to photo"
+              else
+                puts "Error adding geolocation to photo" + plaque.errors.full_messages.to_s #methods.join(" ")
+              end
+            end
+          else
+            puts "Photo's machine tag doesn't match a plaque."
+          end
         end
       end
     end
